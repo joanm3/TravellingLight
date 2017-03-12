45237bc1b8f2c70a57497e9aeb1dbcbaad2ad25d
Drackonis <kergastel@hotmail.fr>
2017-03-12 12:20:34 +0100

    RestartButton

---
+++ Assets/RestartScene.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class RestartScene : MonoBehaviour {
+
+    public int currentScene;
+    public KeyCode restartKey = KeyCode.R;
+
+    void Update()
+    {
+        if (Input.GetKeyDown(restartKey))
+        {
+            SceneManager.LoadScene(currentScene);
+        }
+    }
+
+
+}
