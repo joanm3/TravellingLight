8a7a44b9bac680322e98902025e136c58ff8d47d
Drackonis <kergastel@hotmail.fr>
2017-03-19 17:24:30 +0100

    19/03 Divers

---
+++ Assets/Scripts/ColliderController.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ColliderController : MonoBehaviour {
+
+
+    public enum WorldAppartenance
+    {
+        world1,world2
+    }
+
+    public WorldAppartenance gameObjectWorld;
+    public Collider col; 
+
+    public void DoSomething()
+    {
+        gameObject.GetComponent<Collider>().enabled = !gameObject.GetComponent<Collider>().enabled;
+    }
+
+    //CurrentWorld = true : World 1
+    //CurrentWorld = false : World 2
+    void OnChangeWorld(WorldAppartenance w)
+    {
+        if (w == WorldAppartenance.world1 && gameObjectWorld == WorldAppartenance.world1)
+        {
+            //gameObject.GetComponent<Collider>().
+        }
+    }
+}
---
+++ Assets/Scripts/ColliderManager.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ColliderManager : Singleton<ColliderManager>
+{
+
+
+    public List<ColliderController> colliderControllers; 
+
+
+    void Start()
+    {
+        ColliderController[] _col = FindObjectsOfType<ColliderController>();
+        for (int i = 0; i < _col.Length; i++)
+        {
+            colliderControllers.Add(_col[i]); 
+        }
+    }
+
+    public void ChangeColliders()
+    {
+        for (int i = 0; i < colliderControllers.Count; i++)
+        {
+            colliderControllers[i].DoSomething(); 
+        }
+    }
+	
+}
