1bdb4ab3b19feea3f0565dd5a024eb7db4e345b1
Joan Mitjans <joanm3@hotmail.com>
2017-05-18 01:06:11 +0200

    no message

---
+++ Assets/ResetLevelOnButtonClicked.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class ResetLevelOnButtonClicked : MonoBehaviour
+{
+
+    public KeyCode keyToPress = KeyCode.R;
+
+
+    void Update()
+    {
+        if (Input.GetKeyDown(keyToPress))
+        {
+            SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
+        }
+    }
+}
