0a5510689664492614c09d9557b60c3bc9961a37
Drackonis <kergastel@hotmail.fr>
2017-05-17 23:00:44 +0200

    Menu Scene

---
+++ Assets/MenuScript.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor.SceneManagement;
+
+public class MenuScript : MonoBehaviour {
+
+    public WorldMaskManager bubbleControler;
+    public float openSpeed;
+    bool actived = false;
+    public int sceneNumber;
+
+    void Update()
+    {
+        if (Input.GetButtonDown("Fire1"))
+        {
+            print("ON IT ");
+            actived = true;           
+        }
+
+        if (actived && bubbleControler.forestTargets[0].cloak >= 0)
+        {
+            bubbleControler.forestTargets[0].cloak -= openSpeed * Time.deltaTime;
+        }
+        else
+        {
+            //SceneManager.LoadScene(sceneNumber);
+            //Application.LoadLevel(sceneNumber);
+        }
+    }
+}
